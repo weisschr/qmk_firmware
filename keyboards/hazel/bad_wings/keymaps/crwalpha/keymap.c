fc662da474a9e9670bf8ee6151e1620548ed0c22
Chris Weiss <chris@chrisweiss.com>
2023-09-06 09:59:26 -0400

    TWEAKED COMBOS
    twek

--- keyboards/hazel/bad_wings/keymaps/crwalpha/keymap.c
+++ keyboards/hazel/bad_wings/keymaps/crwalpha/keymap.c
@@ -57,26 +57,21 @@ tap_dance_action_t tap_dance_actions[] = {
 uint16_t COMBO_LEN = 19;
 
 // Layer 0 combos
-const uint16_t PROGMEM cho_combo1[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM cho_combo2[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM cho_combo3[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM cho_combo4[]= {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM cho_combo5[] = {KC_A, KC_S, COMBO_END};
-const uint16_t PROGMEM cho_combo6[] = {KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM cho_combo7[] = {KC_S,KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM cho_combo8[] = {KC_J, KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM cho_combo9[] = {KC_S, KC_F, COMBO_END};
-const uint16_t PROGMEM cho_combo10[] = {KC_J, KC_L, COMBO_END};
-const uint16_t PROGMEM cho_combo11[] = {KC_A, KC_S, KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM cho_combo12[] = {KC_J, KC_K, KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM cho_combo13[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM cho_combo14[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM cho_combo1[] = {LALT_T(KC_D), LCTL_T(KC_F), COMBO_END};
+const uint16_t PROGMEM cho_combo2[] = {RCTL_T(KC_J), RALT_T(KC_K), COMBO_END};
+const uint16_t PROGMEM cho_combo6[] = {RGUI_T(KC_L), KC_SCLN, COMBO_END};
+const uint16_t PROGMEM cho_combo7[] = {LGUI_T(KC_S),LALT_T(KC_D), LCTL_T(KC_F), COMBO_END};
+const uint16_t PROGMEM cho_combo8[] = {RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), COMBO_END};
+const uint16_t PROGMEM cho_combo9[] = {LGUI_T(KC_S), LCTL_T(KC_F), COMBO_END};
+const uint16_t PROGMEM cho_combo10[] = {RCTL_T(KC_J), RGUI_T(KC_L), COMBO_END};
+const uint16_t PROGMEM cho_combo11[] = {KC_A, LGUI_T(KC_S), KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM cho_combo12[] = {RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), KC_SCLN, COMBO_END};
 const uint16_t PROGMEM cho_combo15[] = {KC_X, KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM cho_combo16[] = {KC_M, KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM cho_combo17[] = {KC_Z, KC_X, KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM cho_combo18[] = {KC_M, KC_COMM, KC_DOT, KC_SLSH, COMBO_END};
-const uint16_t PROGMEM cho_combo19[] = {KC_F, KC_G, COMBO_END};
-const uint16_t PROGMEM cho_combo20[] = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM cho_combo19[] = {LCTL_T(KC_F), KC_G, COMBO_END};
+const uint16_t PROGMEM cho_combo20[] = {KC_H, RCTL_T(KC_J), COMBO_END};
 const uint16_t PROGMEM cho_combo21[] = {KC_Y, KC_U, COMBO_END};
 const uint16_t PROGMEM cho_combo22[] = {KC_R, KC_T, COMBO_END};
 const uint16_t PROGMEM cho_combo23[] = {TD(TD_CAPS), KC_B, COMBO_END};
@@ -87,18 +82,12 @@ const uint16_t PROGMEM cho_combo25[] = {KC_TAB, KC_BSPC, COMBO_END};
 combo_t key_combos[] = {  
     COMBO(cho_combo1, KC_LSFT),
     COMBO(cho_combo2, KC_RSFT),
-    COMBO(cho_combo3, KC_LCTL),
-    COMBO(cho_combo4, KC_RCTL),
-    COMBO(cho_combo5, KC_LALT),
-    COMBO(cho_combo6, KC_RALT),
     COMBO(cho_combo7, LSFT(KC_LCTL)),
     COMBO(cho_combo8, RSFT(KC_RCTL)),
     COMBO(cho_combo9, LSFT(KC_LALT)),
     COMBO(cho_combo10, RSFT(KC_RALT)),
     COMBO(cho_combo11, KC_MEH),
     COMBO(cho_combo12, KC_MEH),
-    COMBO(cho_combo13, KC_LGUI),
-    COMBO(cho_combo14, KC_RGUI),
     COMBO(cho_combo15, LSFT(KC_LGUI)),
     COMBO(cho_combo16, RSFT(KC_RGUI)),
     COMBO(cho_combo17, KC_HYPR),
@@ -135,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ALPHA] = LAYOUT_split_3x5_3(
     KC_Q,    KC_W,  KC_E,    KC_R,         KC_T,             KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,
-    KC_A,    KC_S,  KC_D,    KC_F,         KC_G,             KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN,
+    KC_A,    LGUI_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H,    RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), KC_SCLN,
     KC_Z,    KC_X,  KC_C,    TD(TD_CAPS),  KC_B,             KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,
                         TO(1), KC_SPACE,   KC_TAB,           KC_BSPC, KC_ENTER, TO(4)
   ),
@@ -148,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *  |-------------|-------------|-------------|-------------|-------------|   |-------------|-------------|-------------|-------------|-------------|
 *  |      / ?    |      [ {    |      ] }    |      ; :    |      ' "    |   |      - _    |      = +    |    < ,      |      . >    |      \ |    |
 *  '---------------------------|-------------|-------------|-------------|   |-------------|-------------|-------------|---------------------------'
-*                              | UPLAYER     | SPACE       | TAB         |   | BACKSPACE   | ENTER       | DOWNLAYER   |
+*                              | UPLAYER     | KC_TRNS     | KC_TRNS     |   | KC_TRNS     | KC_TRNS     | DOWNLAYER   |
 *                              |_____________|_____________|_____________|   |_____________|_____________|_____________|
 */
 
@@ -156,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM,    KC_AT,           KC_HASH,          KC_DLR,       KC_PERC,       KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LEFT_PAREN, KC_RIGHT_PAREN,
     KC_1,       KC_2,            KC_3,             KC_4,         KC_5,          KC_6,       KC_7,       KC_8,       KC_9,          KC_0,
     KC_SLASH,   KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_SEMICOLON, KC_QUOTE,      KC_MINUS,   KC_EQUAL,   KC_COMMA,   KC_DOT,        KC_BACKSLASH,
-                                      TO(2),      KC_SPACE,      KC_TAB,        KC_BSPC,    KC_ENTER,   TO(0)
+                                      TO(2),       KC_TRNS,      KC_TRNS,       KC_TRNS,    KC_TRNS,   TO(0)
   ),
 
 /*  Layer 2 Function
@@ -167,15 +156,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *  |-------------|-------------|-------------|-------------|-------------|   |-------------|-------------|-------------|-------------|-------------|
 *  |  GUI        | ALT         | CTRL        | SHIFT       |  MEH        |   | HYPER       | SHIFT       | CTRL        | ALT         | GUI  .      |
 *  '---------------------------|-------------|-------------|-------------|   |-------------|-------------|-------------|---------------------------'
-*                              | UPLAYER     | SPACE       | TAB         |   | BACKSPACE   | ENTER       | DOWNLAYER   |
+*                              | UPLAYER     | KC_TRNS     | KC_TRNS     |   | KC_TRNS     | KC_TRNS     | DOWNLAYER   |
 *                              |_____________|_____________|_____________|   |_____________|_____________|_____________|
 */
 
   [_FUNCTION] = LAYOUT_split_3x5_3(
-    KC_NO,      KC_NO,     KC_LEFT,    KC_RIGHT,   KC_F11,        KC_F12,    KC_UP,      KC_DOWN,    KC_NO,      KC_NO,
-    KC_F1,      KC_F2,     KC_F3,      KC_F4,      KC_F5,         KC_F6,     KC_F7,      KC_F8,      KC_F9,      KC_F10,
-    KC_LGUI,    KC_LALT,   KC_LCTL,    KC_LSFT,    KC_MEH,        KC_HYPR,   KC_RSFT,  KC_RCTL,   KC_RALT,    KC_RGUI,
-                            TO(3), KC_SPACE,   KC_TAB,            KC_BSPC,   KC_ENTER,   TO(1)
+    KC_NO,      KC_NO,     KC_LEFT,    KC_RIGHT,   KC_F11,      KC_F12,    KC_UP,      KC_DOWN,    KC_NO,      KC_NO,
+    KC_F1,      KC_F2,     KC_F3,      KC_F4,      KC_F5,       KC_F6,     KC_F7,      KC_F8,      KC_F9,      KC_F10,
+    KC_LGUI,    KC_LALT,   KC_LCTL,    KC_LSFT,    KC_MEH,      KC_HYPR,   KC_RSFT,  KC_RCTL,   KC_RALT,    KC_RGUI,
+                              TO(3),   KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,   TO(1)
   ),
 
 /*  Layer 3 Mouse
@@ -186,7 +175,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *  |-------------|-------------|-------------|-------------|-------------|   |-------------|-------------|-------------|-------------|-------------|
 *  | WWWHOME     |             | LEFT        | RIGHT       | DOWN        |   |WHEEL LEFT   |WHEEL UP     |WHEEL DOWN   |WHEEL RIGHT  | KC_WSTP     |
 *  '---------------------------|-------------|-------------|-------------|   |-------------|-------------|-------------|---------------------------'
-*                              | UPLAYER     | SPACE       | TAB         |   | BACKSPACE   | ENTER       | DOWNLAYER   |
+*                              | UPLAYER     | KC_TRNS     | KC_TRNS     |   | KC_TRNS     | KC_TRNS     | DOWNLAYER   |
 *                              |_____________|_____________|_____________|   |_____________|_____________|_____________|
 */
 
@@ -194,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_WBAK,   KC_NO,      KC_VOLD,     KC_MUTE,    KC_VOLU,       KC_HOME,       KC_PGUP,     KC_MS_UP,      KC_PGDN,        KC_WSCH,
     KC_WFWD,   KC_MS_BTN1, KC_MS_BTN2,  KC_MS_BTN3, KC_UP,         KC_END,        KC_MS_LEFT,  KC_MS_DOWN,    KC_MS_RIGHT,    KC_WREF,
     KC_WHOM,   KC_NO,      KC_LEFT,     KC_RIGHT,   KC_DOWN,       KC_MS_WH_LEFT, KC_MS_WH_UP, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_NO,
-                                 TO(4), KC_SPACE,   KC_TAB,        KC_BSPC,   KC_ENTER,    TO(2)
+                                 TO(4), KC_TRNS,    KC_TRNS,       KC_TRNS,       KC_TRNS,   TO(2)
   ),
 
 /*  Layer 4 APPS AND CONTROL
@@ -205,7 +194,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *  |-------------|-------------|-------------|-------------|-------------|   |-------------|-------------|-------------|-------------|-------------|
 *  |             |             |             |             |             |   |             |             |             |             |             |
 *  '---------------------------|-------------|-------------|-------------|   |-------------|-------------|-------------|---------------------------'
-*                              | UPLAYER     | SPACE       | TAB         |   | BACKSPACE   | ENTER       | DOWNLAYER   |
+*                              | UPLAYER     | KC_TRNS     | KC_TRNS     |   | KC_TRNS     | KC_TRNS     | DOWNLAYER   |
 *                              |_____________|_____________|_____________|   |_____________|_____________|_____________|
 */
 
@@ -213,6 +202,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,      KC_NO,      KC_NO,       KC_NO,      KC_NO,          KC_NO,      QK_BOOT,    QK_REBOOT,  KC_NO,     KC_NO,
     KC_NO,      KC_NO,      KC_NO,       KC_NO,      KC_NO,          KC_CALC,    KC_NO,      KC_NO,      KC_NO,     KC_NO,
     KC_NO,      KC_NO,      KC_NO,       KC_NO,      KC_NO,          KC_NO,      KC_NO,      KC_NO,      KC_NO,     KC_NO,
-                                  TO(0), KC_SPACE,   KC_TAB,         KC_BSPC,    KC_ENTER,   TO(3)
+                                  TO(0), KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,   TO(3)
   )
 };
