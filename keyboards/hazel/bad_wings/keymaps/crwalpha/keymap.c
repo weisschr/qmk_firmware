e0f466d8d7de493b200c3fed496c6ca347201226
Chris Weiss <chris@chrisweiss.com>
2023-09-06 15:58:32 -0400

    Added support for caps word, and eliminated tap dance

--- keyboards/hazel/bad_wings/keymaps/crwalpha/keymap.c
+++ keyboards/hazel/bad_wings/keymaps/crwalpha/keymap.c
@@ -24,6 +24,21 @@ tap_dance_action_t tap_dance_actions[] = {
 uint16_t COMBO_LEN = 19;
 
 // Layer 0 combos
+enum combos {
+    [LSHFT_COMBO],			
+[RSHFT_COMBO],
+[LESC_COMBO],
+[RESC_COMBO],
+[DEL_COMBO],
+[INS_COMBO],
+[WINCLOSE_COMBO],
+[APPCLOSE_COMBO],
+[LAYERNUM_COMBO],
+[LAYERMOUSE_COMBO],
+[CAPLOCK_COMBO],
+[CAPSWORD_COMBO],
+}
+
 const uint16_t PROGMEM lshft_combo[] = {LALT_T(KC_D), LCTL_T(KC_F), COMBO_END};
 const uint16_t PROGMEM rshft_combo[] = {RCTL_T(KC_J), RALT_T(KC_K), COMBO_END};
 const uint16_t PROGMEM lesc_combo[] = {LCTL_T(KC_F), RCS_T(KC_G), COMBO_END};
@@ -32,25 +47,40 @@ const uint16_t PROGMEM del_combo[] = {MEH_T(KC_Y), RSA_T(KC_U), COMBO_END};
 const uint16_t PROGMEM ins_combo[] = {HYPR_T(KC_N), KC_M, COMBO_END};
 const uint16_t PROGMEM winclose_combo[] = {LSA_T(KC_R), MEH_T(KC_T), COMBO_END};
 const uint16_t PROGMEM appclose_combo[] = {TD(TD_CAPS), HYPR_T(KC_B), COMBO_END};
-const uint16_t PROGMEM layernum_combo24[] = {KC_SPACE, KC_ENTER, COMBO_END};
-const uint16_t PROGMEM layermouse_combo25[] = {KC_TAB, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM layernum_combo[] = {KC_SPACE, KC_ENTER, COMBO_END};
+const uint16_t PROGMEM layermouse_combo[] = {KC_TAB, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM caplock_combo[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM capsword_combo[] = {KC_M, KC_COMM, COMBO_END};
 
 // layer 0 combos
 combo_t key_combos[] = {  
-    COMBO(lshft_combo, KC_LSFT),
-    COMBO(rshft_combo, KC_RSFT),
-    COMBO(lesc_combo, KC_ESC),
-    COMBO(resc_combo, KC_ESC),
-    COMBO(del_combo, KC_DEL),
-    COMBO(ins_combo, KC_INS),
-    COMBO(cho_combo22, LCTL(KC_F4)),
-    COMBO(cho_combo23, LALT(KC_F4)),
-    COMBO(cho_combo24, TO(0)),
-    COMBO(cho_combo25, TO(3))
+[LSHFT_COMBO] =		COMBO(lshft_combo, KC_LSFT),
+[RSHFT_COMBO] =     COMBO(rshft_combo, KC_RSFT),
+[LESC_COMBO] =      COMBO(lesc_combo, KC_ESC),
+[RESC_COMBO] =      COMBO(resc_combo, KC_ESC),
+[DEL_COMBO] =       COMBO(del_combo, KC_DEL),
+[INS_COMBO] =       COMBO(ins_combo, KC_INS),
+[WINCLOSE_COMBO] =  COMBO(winclose_combo, LCTL(KC_F4)),
+[APPCLOSE_COMBO] =  COMBO(appclose_combo, LALT(KC_F4)),
+[LAYERNUM_COMBO] =  COMBO(layernum_combo, TO(0)),
+[LAYERMOUSE_COMBO] = COMBO(layermouse_combo, TO(3)),
+[CAPLOCK_COMBO] =   COMBO(caplock_combo,KC_CAPS),
+[CAPSWORD_COMBO] =  COMBO_ACTION(capsword_combo),
 };
 // end combos
 
 // Special behavior code
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  switch(combo_index) {
+    case CAPSWORD_COMBO:
+      if (pressed) {
+        caps_word_toggle();
+      }
+      break;
+  }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
@@ -73,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT_split_3x5_3(
     KC_Q,        KC_W,            KC_E,         LSA_T(KC_R),  MEH_T(KC_T),      MEH_T(KC_Y),    RSA_T(KC_U),  KC_I,         KC_O,         KC_P,
     KC_A,        LGUI_T(KC_S),    LALT_T(KC_D), LCTL_T(KC_F), RCS_T(KC_G),      RCS_T(KC_H),    RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), KC_SCLN,
-    KC_Z,        KC_X,            KC_C,         TD(TD_CAPS),  HYPR_T(KC_B),     HYPR_T(KC_N),   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
+    KC_Z,        KC_X,            KC_C,         KC_V,         HYPR_T(KC_B),     HYPR_T(KC_N),   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
                                   TO(1),        KC_SPACE,     KC_TAB,           KC_BSPC,        KC_ENTER,     TO(4)
   ),
 
