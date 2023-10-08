d527664345004996d283178cd2e7f2a02120346a
Chris Weiss <chris@chrisweiss.com>
2023-10-08 17:40:07 -0400

    Added combos to arrow keys.  Eliminated capsword behavior.  Refined combos

--- keyboards/hazel/bad_wings/keymaps/crwalpha/keymap.c
+++ keyboards/hazel/bad_wings/keymaps/crwalpha/keymap.c
@@ -33,8 +33,10 @@ enum custom_keycodes {
 };
 
 enum combos {
-    LSHFT_COMBO,			
-    RSHFT_COMBO,
+    LARROW_COMBO,
+    RARROW_COMBO,
+    UARROW_COMBO,
+    DARROW_COMBO,
     LESC_COMBO,
     RESC_COMBO,
     DEL_COMBO,
@@ -69,8 +71,10 @@ tap_dance_action_t tap_dance_actions[] = {
 
 // Need to test if shift-enter and control-enter still work
 
-const uint16_t PROGMEM lshft_combo[]      = {LALT_T(KC_D), LCTL_T(KC_F), COMBO_END};
-const uint16_t PROGMEM rshft_combo[]      = {RCTL_T(KC_J), RALT_T(KC_K), COMBO_END};
+const uint16_t PROGMEM larrow_combo[]      = {LALT_T(KC_D), LCTL_T(KC_F), COMBO_END};
+const uint16_t PROGMEM rarrow_combo[]      = {RCTL_T(KC_J), RALT_T(KC_K), COMBO_END};
+const uint16_t PROGMEM darrow_combo[]      = {LGUI_T(KC_S),  LALT_T(KC_D), COMBO_END};
+const uint16_t PROGMEM uarrow_combo[]      = {RALT_T(KC_K), RGUI_T(KC_L), COMBO_END};
 const uint16_t PROGMEM lesc_combo[]       = {LCTL_T(KC_F), RCS_T(KC_G), COMBO_END};
 const uint16_t PROGMEM resc_combo[]       = {RCS_T(KC_H), RCTL_T(KC_J), COMBO_END};
 const uint16_t PROGMEM del_combo[]        = {MEH_T(KC_Y), RSA_T(KC_U), COMBO_END};
@@ -78,7 +82,6 @@ const uint16_t PROGMEM ins_combo[]        = {HYPR_T(KC_N), KC_M, COMBO_END};
 const uint16_t PROGMEM winclose_combo[]   = {LSA_T(KC_R), MEH_T(KC_T), COMBO_END};
 const uint16_t PROGMEM appclose_combo[]   = {KC_V, HYPR_T(KC_B), COMBO_END};
 const uint16_t PROGMEM caplock_combo[]    = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM capsword_combo[]   = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM osl_sym_combo[]    = {KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM osl_func_combo[]   = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM osl_aspp_combo[]   = {KC_X, KC_V, COMBO_END};
@@ -91,8 +94,10 @@ Add combos for ' " and arrows
 
 // layer 0 combos
 combo_t key_combos[] = {  
-[LSHFT_COMBO] =	  	   COMBO(lshft_combo, KC_LSFT),
-[RSHFT_COMBO] =        COMBO(rshft_combo, KC_RSFT),
+[LARROW_COMBO] =	  	 COMBO(larrow_combo, KC_LEFT),
+[RARROW_COMBO] =       COMBO(rarrow_combo, KC_RIGHT),
+[UARROW_COMBO] =	  	 COMBO(uarrow_combo, KC_UP),
+[DARROW_COMBO] =       COMBO(darrow_combo, KC_DOWN),
 [LESC_COMBO] =         COMBO(lesc_combo, KC_ESC),
 [RESC_COMBO] =         COMBO(resc_combo, KC_ESC),
 [DEL_COMBO] =          COMBO(del_combo, KC_DEL),
@@ -100,22 +105,11 @@ combo_t key_combos[] = {
 [WINCLOSE_COMBO] =     COMBO(winclose_combo, LCTL(KC_F4)),
 [APPCLOSE_COMBO] =     COMBO(appclose_combo, LALT(KC_F4)),
 [CAPLOCK_COMBO] =      COMBO(caplock_combo,KC_CAPS),
-[CAPSWORD_COMBO] =     COMBO_ACTION(capsword_combo),
 [ONESHOT_SYM_COMBO] =  COMBO(osl_sym_combo, OSL(_NUMBSYM)),
 [ONESHOT_FUNC_COMBO] = COMBO(osl_func_combo, OSL(_FUNCTION)),
 [ONESHOT_APP_COMBO] =  COMBO(osl_aspp_combo, OSL(_APPCONTROL)),
 };
 // end combos
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case CAPSWORD_COMBO:
-      if (pressed) {
-        caps_word_toggle();
-      }
-      break;
-  }
-}
 
 static bool space_shift_active = false;
 static bool enter_shift_active = false;
@@ -123,7 +117,6 @@ static bool space_shift_held = false;
 static bool enter_shift_held = false;
 static uint16_t key_timer_default;
 static uint16_t key_timer_mouse;
-
 
 void handle_custom_shift(keyrecord_t *record, bool *custom_shift_held, bool *shift_active, uint16_t kc) {
   if (record->event.pressed) {
@@ -208,7 +201,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,  KC_W,          KC_E,         LSA_T(KC_R),   MEH_T(KC_T),      MEH_T(KC_Y),  RSA_T(KC_U),  KC_I,         KC_O,         KC_P,
     KC_A,  LGUI_T(KC_S),  LALT_T(KC_D), LCTL_T(KC_F),  RCS_T(KC_G),      RCS_T(KC_H),  RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), KC_SCLN,
     KC_Z,  KC_X,          KC_C,         KC_V,          HYPR_T(KC_B),     HYPR_T(KC_N), KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
-                          TO(_NUMBSYM),     SPACE_SHIFT,   TAB_ALPHA,        BKSPC_MOUSE,  ENTER_SHIFT,  TO(_APPCONTROL)
+                          TO(_NUMBSYM), SPACE_SHIFT,   TAB_ALPHA,        BKSPC_MOUSE,  ENTER_SHIFT,  TO(_APPCONTROL)
   ),
 
 /*  Layer 1 Symbol
@@ -225,10 +218,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
   [_NUMBSYM] = LAYOUT_split_3x5_3(
-    KC_EXLM,      KC_AT,   KC_HASH,   KC_DLR,    KC_PERC,         KC_CIRC,  KC_AMPR,   KC_ASTR,         KC_LEFT_PAREN,  KC_RIGHT_PAREN,
-    KC_1,         KC_2,    KC_3,      KC_4,      KC_5,            KC_6,     KC_7,      KC_8,             KC_9,          KC_0,
-    TD(TD_GRAVE), KC_PLUS, KC_EQUAL,  KC_MINUS,  KC_UNDS,         KC_DQUO,  KC_QUOTE,  TD(TD_BACKSLASH), TD(TD_LBRACE), TD(TD_RBRACE),
-                           TO(_FUNCTION),   KC_TRNS,   KC_TRNS,         KC_TRNS,  KC_TRNS,   TO(_ALPHA)
+    KC_EXLM,      KC_AT,   KC_HASH,       KC_DLR,    KC_PERC,         KC_CIRC,  KC_AMPR,   KC_ASTR,         KC_LEFT_PAREN,  KC_RIGHT_PAREN,
+    KC_1,         KC_2,    KC_3,          KC_4,      KC_5,            KC_6,     KC_7,      KC_8,             KC_9,          KC_0,
+    TD(TD_GRAVE), KC_PLUS, KC_EQUAL,      KC_MINUS,  KC_UNDS,         KC_DQUO,  KC_QUOTE,  TD(TD_BACKSLASH), TD(TD_LBRACE), TD(TD_RBRACE),
+                           TO(_FUNCTION), KC_TRNS,   KC_TRNS,         KC_TRNS,  KC_TRNS,   TO(_ALPHA)
   ),
 
 /*  Layer 2 Function
@@ -246,10 +239,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
   [_FUNCTION] = LAYOUT_split_3x5_3(
-    KC_END,  KC_HOME, KC_LEFT,  KC_RIGHT,  KC_F11,      KC_F12,    KC_UP,    KC_DOWN,   KC_PGUP,   KC_PGDN,
-    KC_F1,   KC_F2,   KC_F3,    KC_F4,     KC_F5,       KC_F6,     KC_F7,    KC_F8,     KC_F9,   KC_F10,
-    KC_LGUI, KC_LALT, KC_LCTL,  KC_LSFT,   KC_MEH,      KC_HYPR,   KC_RSFT,  KC_RCTL,   KC_RALT, KC_RGUI,
-                      TO(_MOUSE),  KC_TRNS,   KC_TRNS,     KC_TRNS,   KC_TRNS,  TO(_NUMBSYM)
+    KC_END,  KC_HOME, KC_LEFT,    KC_RIGHT,  KC_F11,      KC_F12,    KC_UP,    KC_DOWN,   KC_PGUP,   KC_PGDN,
+    KC_F1,   KC_F2,   KC_F3,      KC_F4,     KC_F5,       KC_F6,     KC_F7,    KC_F8,     KC_F9,   KC_F10,
+    KC_LGUI, KC_LALT, KC_LCTL,    KC_LSFT,   KC_MEH,      KC_HYPR,   KC_RSFT,  KC_RCTL,   KC_RALT, KC_RGUI,
+                      TO(_MOUSE), KC_TRNS,   KC_TRNS,     KC_TRNS,   KC_TRNS,  TO(_NUMBSYM)
   ),
 
 /*  Layer 3 Mouse
@@ -265,10 +258,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
   [_MOUSE] = LAYOUT_split_3x5_3(
-    KC_WBAK,   C(KC_T),     KC_VOLD,    KC_MUTE,    KC_VOLU,      RSFT_T(KC_HOME), KC_PGUP,     KC_MS_UP,      KC_PGDN,        KC_WSCH,
-    KC_WFWD,   KC_MS_BTN1,  KC_MS_BTN3, KC_MS_BTN2, KC_UP,        RCTL_T(KC_END),  KC_MS_LEFT,  KC_MS_DOWN,    KC_MS_RIGHT,    KC_WREF,
-    KC_WHOM,   C(KC_N),     KC_LEFT,    KC_RIGHT,   KC_DOWN,      KC_MS_WH_LEFT,   KC_MS_WH_UP, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_WSTP,
-                            TO(_APPCONTROL),    KC_TRNS,    KC_TRNS,      KC_TRNS,         KC_TRNS,     TO(_FUNCTION)
+    KC_WBAK,   C(KC_T),     KC_VOLD,          KC_MUTE,    KC_VOLU,      RSFT_T(KC_HOME), KC_PGUP,     KC_MS_UP,      KC_PGDN,        KC_WSCH,
+    KC_WFWD,   KC_MS_BTN1,  KC_MS_BTN3,       KC_MS_BTN2, KC_UP,        RCTL_T(KC_END),  KC_MS_LEFT,  KC_MS_DOWN,    KC_MS_RIGHT,    KC_WREF,
+    KC_WHOM,   C(KC_N),     KC_LEFT,          KC_RIGHT,   KC_DOWN,      KC_MS_WH_LEFT,   KC_MS_WH_UP, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_WSTP,
+                            TO(_APPCONTROL),  KC_TRNS,    KC_TRNS,      KC_TRNS,         KC_TRNS,     TO(_FUNCTION)
   ),
 
 /*  Layer 4 APPS AND CONTROL
@@ -287,6 +280,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,      KC_NO,   HYPR_T(KC_L), HYPR_T(KC_O),   HYPR_T(KC_T),     RGUI(KC_E),  QK_BOOT,     QK_REBOOT,  KC_NO,     KC_NO,
     KC_NO,      KC_NO,   KC_NO,        HYPR_T(KC_P),   HYPR_T(KC_W),     KC_CALC,     RCS(KC_ESC), RGUI(KC_R), KC_NO,     KC_NO,
     KC_NO,      KC_NO,   KC_NO,        LGUI(KC_DOT),   HYPR_T(KC_X),     SGUI(KC_S),  RGUI(KC_V),  KC_NO,      KC_NO,     KC_NO,
-                         TO(_ALPHA),      KC_TRNS,        KC_TRNS,          KC_TRNS,     KC_TRNS,     TO(_MOUSE)
+                         TO(_ALPHA),   KC_TRNS,        KC_TRNS,          KC_TRNS,     KC_TRNS,     TO(_MOUSE)
   )
 };
--- keyboards/hazel/bad_wings/keymaps/crwalpha/rules.mk
+++ keyboards/hazel/bad_wings/keymaps/crwalpha/rules.mk
@@ -3,3 +3,4 @@ EXTRAKEY_ENABLE			= yes
 COMBO_ENABLE			= yes
 TAP_DANCE_ENABLE		= yes
 CAPS_WORD_ENABLE        = yes
+SEND_STRING_ENABLE      = yes
