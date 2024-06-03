f875c9441b2f3d22ded143998f70b6e70f688d7b
Chris Weiss <chris@chrisweiss.com>
2024-06-02 22:49:00 -0400

    Next iteration of keymap

---
+++ keyboards/keebio/iris_ce/keymaps/crwalpha/config.h
@@ -0,0 +1,2 @@
+#define TAPPING_TERM 200
+#define COMBO_TERM 50
--- keyboards/keebio/iris_ce/keymaps/crwalpha/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/crwalpha/keymap.c
@@ -3,12 +3,148 @@
 
 #include QMK_KEYBOARD_H
 
-enum custom_layers {
+#define KA_LINKEDIN HYPR(KC_L)
+#define KA_OUTLOOK  HYPR(KC_O)
+#define KA_TEAMS    HYPR(KC_T)
+#define KA_FILEMGR  RGUI(KC_E)
+#define KA_POWERPT  HYPR(KC_P)
+#define KA_WORD     HYPR(KC_W)
+#define KA_TASKMGR  RCS(KC_ESC)
+#define KA_RUN      RGUI(KC_R)
+#define KA_EMOJIS   LGUI(KC_DOT)
+#define KA_EXCEL    HYPR(KC_X)
+#define KA_SNIP     SGUI(KC_S)
+#define KA_CLIPBRD  RGUI(KC_V)
+
+//-------------------------------------------------------------------------------
+
+enum layers {
      _DEFAULT,
      _MOUSE,
      _APPS,
      _COLEMAKDH
 };
+
+enum combos {
+    WINCLOSE_COMBO,
+    APPCLOSE_COMBO,
+    CAPLOCK_COMBO,
+    LEFTARROW_COMBO,
+    RIGHTARROW_COMBO,
+    UPARROW_COMBO,
+    DOWNARROW_COMBO,
+    PAGEUP_COMBO,
+    PAGEDN_COMBO,
+    HOME_COMBO,
+    END_COMBO,
+    COMBO_LENGTH
+};
+
+enum custom_keycodes {
+    BROWSWEROPEN = SAFE_RANGE,
+    CONTROLPAN,
+    MYCOMPUTER,
+    CUSTOM_LENGTH
+};
+
+const uint16_t PROGMEM winclose_combo[]   = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM appclose_combo[]   = {KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM caplock_combo[]    = {KC_Z, KC_X, KC_C,  COMBO_END};
+const uint16_t PROGMEM leftarrow_combo[]  = {KC_D, KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM rightarrow_combo[] = {KC_E, KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM uparrow_combo[]    = {KC_W, KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM downarrow_combo[]  = {KC_S, KC_D, KC_F, COMBO_END};
+
+const uint16_t PROGMEM pageup_combo[]     = {KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM pagedn_combo[]     = {KC_V, KC_M, COMBO_END};
+const uint16_t PROGMEM home_combo[]       = {KC_G, KC_H, COMBO_END};
+const uint16_t PROGMEM end_combo[]        = {KC_B, KC_N, COMBO_END};
+
+const uint16_t PROGMEM teams_mute[]       = {KC_R, KC_U, COMBO_END};
+const uint16_t PROGMEM teams_vidtog[]     = {KC_T, KC_Y, COMBO_END};
+
+
+combo_t key_combos[] = {
+
+[WINCLOSE_COMBO]     = COMBO(winclose_combo, LCTL(KC_F4)),
+[APPCLOSE_COMBO]     = COMBO(appclose_combo, LALT(KC_F4)),
+[LEFTARROW_COMBO]    = COMBO(leftarrow_combo,KC_LEFT),
+[RIGHTARROW_COMBO]   = COMBO(rightarrow_combo,KC_RIGHT),
+[UPARROW_COMBO]      = COMBO(uparrow_combo, KC_UP),
+[DOWNARROW_COMBO]    = COMBO(downarrow_combo, KC_DOWN),
+[PAGEUP_COMBO]       = COMBO(pageup_combo, KC_PGUP),
+[PAGEDN_COMBO]       = COMBO(pagedn_combo, KC_PGDN),
+[HOME_COMBO]         = COMBO(home_combo, KC_HOME),
+[END_COMBO]          = COMBO(end_combo, KC_END),
+
+//-------------------------------------------------------------------------------
+
+[CAPLOCK_COMBO]      = COMBO(caplock_combo, KC_CAPS),
+
+};
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_BSPC:
+        case KC_DEL:
+            return true;
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_SPACE):
+        case RSFT_T(KC_ENTER):
+            // Immediately select the hold action when another key is pressed.
+             return true;
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
+}
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case BROWSWEROPEN:
+        if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI));
+            SEND_STRING(SS_TAP(X_R));
+            SEND_STRING(SS_UP(X_LGUI));
+            SEND_STRING("CHROME\n");
+        }
+        break;
+    case CONTROLPAN:
+        if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI));
+            SEND_STRING(SS_TAP(X_R));
+            SEND_STRING(SS_UP(X_LGUI));
+            SEND_STRING("CONTROL\n");
+        }
+        break;
+    case MYCOMPUTER:
+        if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI));
+            SEND_STRING(SS_TAP(X_R));
+            SEND_STRING(SS_UP(X_LGUI));
+            SEND_STRING("SHELL:MyComputerFolder\n");
+        }
+        break;
+    default:
+      return true; // Process all other keycodes normally.
+  }
+  return true;
+}
+
+//-------------------------------------------------------------------------------
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
