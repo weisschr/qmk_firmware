2b86950360b28228c9871f6db74cfc1313265744
Chris Weiss <chris@chrisweiss.com>
2024-07-14 13:05:05 -0400

    Added bilateral combo support

--- keyboards/keebio/iris_ce/keymaps/crwalpha/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/crwalpha/keymap.c
@@ -47,6 +47,13 @@
 #define KA_LSUB   HYPR_T(KC_B)
 #define KA_RSUN   HYPR_T(KC_N)
 
+const uint16_t lf_keylist[] = {KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_LBRC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_EQL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ESC, KC_TAB, KC_SPACE};
+const uint16_t rt_keylist[] = {KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_ENTER, KC_BSPC, KC_DEL};
+
+
+const int rt_keylist_size = sizeof(rt_keylist) / sizeof(rt_keylist[0]);
+const int lf_keylist_size = sizeof(lf_keylist) / sizeof(lf_keylist[0]);
+
 //-------------------------------------------------------------------------------
 
 enum layers {
@@ -122,6 +129,12 @@ void ms_reset (tap_dance_state_t *state, void *user_data);
 void ap_finished (tap_dance_state_t *state, void *user_data);
 void ap_reset (tap_dance_state_t *state, void *user_data);
 
+// Functions for bilateral combos
+bool is_left(uint16_t keycode);
+bool is_right(uint16_t keycode);
+bool is_left_mod(uint8_t mod_state);
+bool is_right_mod(uint8_t mod_state);
+
 const uint16_t PROGMEM winclose_combo[]   = {KC_R, KA_LGUT, COMBO_END};
 const uint16_t PROGMEM appclose_combo[]   = {KC_V, KA_LSUB, COMBO_END};
 const uint16_t PROGMEM caplock_combo[]    = {KC_Z, KC_X, KC_C,  COMBO_END};
@@ -190,6 +203,46 @@ bool caps_word_press_user(uint16_t keycode) {
     }
 }
 
+bool is_left(uint16_t keycode) {
+    for (int i = 0; i < lf_keylist_size; i++) {
+        if (keycode == lf_keylist[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool is_right(uint16_t keycode) {
+    for (int i = 0; i < rt_keylist_size; i++) {
+        if (keycode == rt_keylist[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool is_left_mod(uint8_t mod_state){
+    if ((mod_state & MOD_BIT(KC_LALT)) ||
+        (mod_state & MOD_BIT(KC_LSFT)) ||
+        (mod_state & MOD_BIT(KC_LCTL)) ||
+        (mod_state & MOD_BIT(KC_LGUI))) {
+            return true;
+        };
+
+    return false;
+}
+
+bool is_right_mod(uint8_t mod_state){
+    if ((mod_state & MOD_BIT(KC_RALT)) ||
+        (mod_state & MOD_BIT(KC_RSFT)) ||
+        (mod_state & MOD_BIT(KC_RCTL)) ||
+        (mod_state & MOD_BIT(KC_RGUI))) {
+            return true;
+        };
+
+    return false;
+}
+
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LSFT_T(KC_SPACE):
@@ -216,59 +269,75 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+uint8_t mod_state;
+bool left_mod;
+bool right_mod;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case KM_CHROME:
+
+    mod_state = get_mods();
+
+    if (mod_state) {
         if (record->event.pressed) {
-            SEND_STRING(SS_LGUI("r"));
-            wait_ms(500);
-            SEND_STRING("chrome" SS_TAP(X_ENTER));
+            if((is_left(keycode) && is_left_mod(mod_state)) ||
+               (is_right(keycode) && is_right_mod(mod_state)))
+               {
+                    clear_mods();
+               }
         }
+    }
+
+    switch (keycode) {
+        case KM_CHROME:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r"));
+                wait_ms(500);
+                SEND_STRING("chrome" SS_TAP(X_ENTER));
+            }
+            return false;
+
+        case KM_MYCOMPUTER:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r"));
+                wait_ms(500);
+                SEND_STRING("Shell:MyComputerFolder" SS_TAP(X_ENTER));
+            }
+            return false;
+
+        case KM_CONTROLPAN:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r"));
+                wait_ms(500);
+                SEND_STRING("Control" SS_TAP(X_ENTER));
+            }
+            return false;
+
+        case KM_EDGE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r"));
+                wait_ms(500);
+                SEND_STRING("microsoft-edge:" SS_TAP(X_ENTER));
+            }
+            return false;
+
+        case KM_VSCODE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r"));
+                wait_ms(500);
+                SEND_STRING("code" SS_TAP(X_ENTER));
+            }
         return false;
 
-    case KM_MYCOMPUTER:
-        if (record->event.pressed) {
-            SEND_STRING(SS_LGUI("r"));
-            wait_ms(500);
-            SEND_STRING("Shell:MyComputerFolder" SS_TAP(X_ENTER));
-        }
-        return false;
+        case KM_NOTEPADPP:
+            if (record->event.pressed) {
+              SEND_STRING(SS_LGUI("r"));
+                wait_ms(500);
+                SEND_STRING("notepad++" SS_TAP(X_ENTER));
+            }
+            return false;
 
-    case KM_CONTROLPAN:
-        if (record->event.pressed) {
-            SEND_STRING(SS_LGUI("r"));
-            wait_ms(500);
-            SEND_STRING("Control" SS_TAP(X_ENTER));
-        }
-        return false;
-
-    case KM_EDGE:
-        if (record->event.pressed) {
-            SEND_STRING(SS_LGUI("r"));
-            wait_ms(500);
-            SEND_STRING("microsoft-edge:" SS_TAP(X_ENTER));
-        }
-        return false;
-
-    case KM_VSCODE:
-        if (record->event.pressed) {
-            SEND_STRING(SS_LGUI("r"));
-            wait_ms(500);
-            SEND_STRING("code" SS_TAP(X_ENTER));
-        }
-        return false;
-
-    case KM_NOTEPADPP:
-        if (record->event.pressed) {
-            SEND_STRING(SS_LGUI("r"));
-            wait_ms(500);
-            SEND_STRING("notepad++" SS_TAP(X_ENTER));
-        }
-        return false;
-
-    default:
-
-        return true; // Process all other keycodes normally.
+        default:
+            return true; // Process all other keycodes normally.
   }
   return true;
 }
